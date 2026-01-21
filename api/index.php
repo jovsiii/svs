a7f8e698c9fdd7bbe1cea093ba7d7f62acaee6b0
Jovan Conde <jovandconde@iskolarngbayan.pup.edu.ph>
2026-01-21 12:59:02 +0800

    Add Vercel deployment configuration

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$kernel->bootstrap();
+
+$request = Illuminate\Http\Request::capture();
+
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
